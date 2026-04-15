2b4a8ca3b060b064fac8f0ca47c48114439f125b
explorer02 <johncartor27@gmail.com>
2026-04-16 00:01:40 +0530

    fix: upgrade to next 16

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@explorer02/api-playground'],
-  webpack: config => {
-    config.resolve.alias['~'] = path.resolve(__dirname, 'packages/api-playground/src');
-    config.resolve.conditionNames = ['source', 'import', 'require', '...'];
-    return config;
+  turbopack: {
+    resolveAlias: {
+      '~': path.resolve(__dirname, 'packages/api-playground/src'),
+    },
+    resolveConditions: ['source', 'import', 'require'],
   },
 };
 
